5f1b7e92356e04fb599588fdd469edc3657971dd
John Blum <jblum@vmware.com>
2021-11-09 17:03:07 -0800

    Edit Javadoc of the HelloWorld class in the simple-java-examples module.

--- simple-java-examples/src/main/java/io/examples/app/hello/HelloWorld.java
+++ simple-java-examples/src/main/java/io/examples/app/hello/HelloWorld.java
@@ -1,7 +1,8 @@
 package io.examples.app.hello;
 
 /**
- * The {@link HelloWorld} class is a simple Java program that prints {@literal Hello World} to standard out.
+ * The {@link HelloWorld} class is a simple Java program that prints {@literal Hello World}
+ * to {@link System#out standard out}.
  *
  * @author John Blum
  * @since 1.0.0
