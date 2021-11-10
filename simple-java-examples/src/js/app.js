a3e36f099ffb2719e86e11e49c61faedcf42a2e7
John Blum <jblum@vmware.com>
2021-11-09 17:04:17 -0800

    Format JavaScript app.js source code in the simple-java-examples module.

--- simple-java-examples/src/js/app.js
+++ simple-java-examples/src/js/app.js
@@ -4,7 +4,7 @@ const span = require("ansispan");
 require("colors");
 
 http.createServer(function(request, response) {
-  response.writeHead(200, { "Content-Type": "text/html"});
+  response.writeHead(200, { "Content-Type": "text/html" });
   response.end(span("Hello Graal.js!".green));
 }).listen(8000, function() { "Graal.js server running at http://localhost:8000".red; });
 
