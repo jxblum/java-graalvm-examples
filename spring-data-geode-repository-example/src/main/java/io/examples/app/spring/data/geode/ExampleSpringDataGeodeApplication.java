5e9329fa0cc9ffad60ae23b58c7b1ae77331e3ed
John Blum <jblum@vmware.com>
2021-11-10 12:22:59 -0800

    Add example spring-data-geode-repository-example module using SDG with Apache Geode and Spring Data Repositories.

---
+++ spring-data-geode-repository-example/src/main/java/io/examples/app/spring/data/geode/ExampleSpringDataGeodeApplication.java
@@ -0,0 +1,187 @@
+package io.examples.app.spring.data.geode;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.Serializable;
+
+import org.apache.geode.cache.GemFireCache;
+import org.apache.geode.cache.client.ClientRegionShortcut;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.gemfire.client.ClientRegionFactoryBean;
+import org.springframework.data.gemfire.config.annotation.ClientCacheApplication;
+import org.springframework.data.gemfire.config.annotation.EnableEntityDefinedRegions;
+import org.springframework.data.gemfire.mapping.GemfireMappingContext;
+import org.springframework.data.gemfire.mapping.annotation.Region;
+import org.springframework.data.gemfire.repository.config.EnableGemfireRepositories;
+import org.springframework.data.gemfire.repository.support.GemfireRepositoryFactoryBean;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
+import org.springframework.util.Assert;
+
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Example Spring Data for Apache Geode (SDG) Java application (program) that uses Spring Data
+ * {@link CrudRepository Repositories} to access (store/persist and read) data from Apache Geode.
+ *
+ * @author John Blum
+ * @see org.apache.geode.cache.GemFireCache
+ * @see org.apache.geode.cache.Region
+ * @see org.springframework.context.ConfigurableApplicationContext
+ * @see org.springframework.context.annotation.Bean
+ * @see org.springframework.context.annotation.Configuration
+ * @see org.springframework.context.annotation.Import
+ * @see org.springframework.context.annotation.Profile
+ * @see org.springframework.context.annotation.AnnotationConfigApplicationContext
+ * @see org.springframework.data.gemfire.client.ClientRegionFactoryBean
+ * @see org.springframework.data.gemfire.config.annotation.ClientCacheApplication
+ * @see org.springframework.data.gemfire.mapping.GemfireMappingContext
+ * @see org.springframework.data.gemfire.repository.config.EnableGemfireRepositories
+ * @see org.springframework.data.repository.CrudRepository
+ * @since 1.0.0
+ */
+@SuppressWarnings("unused")
+public class ExampleSpringDataGeodeApplication implements Runnable {
+
+	private static final String ACTIVE_SPRING_PROFILE = "annotation-configuration";
+
+	public static void main(String[] args) {
+		new ExampleSpringDataGeodeApplication(args).run();
+	}
+
+	@ClientCacheApplication(name = "ExampleSpringDataGeodeApplicationUsingRepositories", copyOnRead = true)
+	@Import({ AnnotationApplicationConfiguration.class, JavaApplicationConfiguration.class })
+	static class ApplicationConfiguration { }
+
+	@Configuration
+	@Profile("annotation-configuration")
+	@EnableEntityDefinedRegions(basePackageClasses = User.class, clientRegionShortcut = ClientRegionShortcut.LOCAL)
+	@EnableGemfireRepositories(basePackageClasses = UserRepository.class)
+	static class AnnotationApplicationConfiguration { }
+
+	@Configuration
+	@Profile("java-configuration")
+	static class JavaApplicationConfiguration {
+
+		@Bean("Users")
+		ClientRegionFactoryBean<Object, Object> usersRegion(GemFireCache gemfireCache) {
+
+			ClientRegionFactoryBean<Object, Object> usersRegion = new ClientRegionFactoryBean<>();
+
+			usersRegion.setCache(gemfireCache);
+			usersRegion.setShortcut(ClientRegionShortcut.LOCAL);
+
+			return usersRegion;
+		}
+
+		@Bean
+		GemfireRepositoryFactoryBean<CrudRepository<User, Long>, User, Long> userRepository() {
+
+			GemfireRepositoryFactoryBean<CrudRepository<User, Long>, User, Long> userRepository =
+				new GemfireRepositoryFactoryBean<>(UserRepository.class);
+
+			userRepository.setGemfireMappingContext(new GemfireMappingContext());
+
+			return userRepository;
+		}
+	}
+
+	private static @NonNull ConfigurableApplicationContext newApplicationContext(Class<?>... configurationClasses) {
+
+		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+
+		applicationContext.getEnvironment().addActiveProfile(ACTIVE_SPRING_PROFILE);
+		applicationContext.register(configurationClasses);
+		applicationContext.registerShutdownHook();
+		applicationContext.refresh();
+
+		return applicationContext;
+	}
+
+	@Getter(AccessLevel.PROTECTED)
+	private final ConfigurableApplicationContext applicationContext;
+
+	@Getter(AccessLevel.PROTECTED)
+	private final String[] arguments;
+
+	public ExampleSpringDataGeodeApplication(@NonNull String... arguments) {
+		this(newApplicationContext(ApplicationConfiguration.class), arguments);
+	}
+
+	public ExampleSpringDataGeodeApplication(@NonNull ConfigurableApplicationContext applicationContext,
+			@NonNull String... arguments) {
+
+		Assert.notNull(applicationContext, "Spring ApplicationContext is required");
+		Assert.notNull(arguments, "Program arguments are required");
+
+		this.applicationContext = applicationContext;
+		this.arguments = arguments;
+	}
+
+	protected void log(String message, Object... arguments) {
+		System.out.printf(message, arguments);
+		System.out.flush();
+	}
+
+	@Override
+	public void run() {
+		run(getApplicationContext().getBean(UserRepository.class), getArguments());
+	}
+
+	protected void run(@NonNull UserRepository userRepository, @NonNull String[] arguments) {
+
+		User jonDoe = User.as("jonDoe").identifiedBy(1L);
+
+		log("Saving User [%s]...%n", jonDoe);
+
+		userRepository.save(jonDoe);
+
+		log("Finding User [%s] by ID [%d]...%n", jonDoe, jonDoe.getId());
+
+		User loadedJonDoe = userRepository.findById(jonDoe.getId()).orElse(null);
+
+		assertThat(loadedJonDoe).isNotNull();
+		assertThat(loadedJonDoe).isNotSameAs(jonDoe);
+		assertThat(loadedJonDoe).isEqualTo(jonDoe);
+
+		log("SUCCESS!!%n");
+	}
+}
+
+@Getter
+@Region("Users")
+@EqualsAndHashCode
+@RequiredArgsConstructor(staticName = "as")
+class User implements Serializable {
+
+	@Id
+	@Setter(AccessLevel.PRIVATE)
+	private Long id;
+
+	@lombok.NonNull
+	private final String name;
+
+	@NonNull User identifiedBy(@Nullable Long id) {
+		setId(id);
+		return this;
+	}
+
+	@Override
+	public String toString() {
+		return getName();
+	}
+}
+
+interface UserRepository extends CrudRepository<User, Long> { }
