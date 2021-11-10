4ed6f24b2253a9306ebe06cb6058d7970ac5b02e
John Blum <jblum@vmware.com>
2021-11-09 17:33:09 -0800

    Refactor the ExampleSpringDataGeodeApplication class.
    Edit Javadoc.

--- spring-data-geode-template-example/src/main/java/io/examples/app/spring/data/geode/ExampleSpringDataGeodeApplication.java
+++ spring-data-geode-template-example/src/main/java/io/examples/app/spring/data/geode/ExampleSpringDataGeodeApplication.java
@@ -32,10 +32,10 @@ import lombok.Setter;
  *
  * @author John Blum
  * @see org.apache.geode.cache.GemFireCache
+ * @see org.apache.geode.cache.Region
  * @see org.springframework.context.ConfigurableApplicationContext
  * @see org.springframework.context.annotation.AnnotationConfigApplicationContext
  * @see org.springframework.context.annotation.Bean
- * @see org.springframework.context.annotation.Configuration
  * @see org.springframework.data.gemfire.GemfireTemplate
  * @see org.springframework.data.gemfire.config.annotation.ClientCacheApplication
  * @see org.springframework.data.gemfire.config.annotation.EnableEntityDefinedRegions
@@ -48,72 +48,6 @@ public class ExampleSpringDataGeodeApplication implements Runnable {
 
 	public static void main(String[] args) {
 		new ExampleSpringDataGeodeApplication(args).run();
-	}
-
-	private static @NonNull ConfigurableApplicationContext newApplicationContext(Class<?>... configurationClases) {
-
-		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
-
-		applicationContext.register(configurationClases);
-		applicationContext.registerShutdownHook();
-		applicationContext.refresh();
-
-		return applicationContext;
-	}
-
-	private final ConfigurableApplicationContext applicationContext;
-
-	private final String[] arguments;
-
-	public ExampleSpringDataGeodeApplication(@NonNull String... arguments) {
-		this(newApplicationContext(ApplicationConfiguration.class), arguments);
-	}
-
-	public ExampleSpringDataGeodeApplication(@NonNull ConfigurableApplicationContext applicationContext,
-			@NonNull String... arguments) {
-
-		Assert.notNull(applicationContext, "Spring ApplicationContext is required");
-
-		this.applicationContext = applicationContext;
-		this.arguments = arguments;
-	}
-
-
-	protected @NonNull ConfigurableApplicationContext getApplicationContext() {
-		return this.applicationContext;
-	}
-
-	protected @NonNull String[] getArguments() {
-		return this.arguments != null ? this.arguments : new String[0];
-	}
-
-	protected void log(String message, Object... arguments) {
-		System.out.printf(message, arguments);
-		System.out.flush();
-	}
-
-	@Override
-	public void run() {
-		run(getApplicationContext().getBean(GemfireTemplate.class), getArguments());
-	}
-
-	protected void run(@NonNull GemfireTemplate usersTemplate, @NonNull String[] arguments) {
-
-		User jonDoe = User.of("jonDoe").identifiedBy(1L);
-
-		log("Saving User [%s]...%n", jonDoe);
-
-		usersTemplate.put(jonDoe.getId(), jonDoe);
-
-		log("Finding User [%s] by ID [%d]...%n", jonDoe, jonDoe.getId());
-
-		User loadedJonDoe = usersTemplate.get(jonDoe.getId());
-
-		assertThat(loadedJonDoe).isNotNull();
-		assertThat(loadedJonDoe).isNotSameAs(jonDoe);
-		assertThat(loadedJonDoe).isEqualTo(jonDoe);
-
-		log("SUCCESS!!%n");
 	}
 
 	@ClientCacheApplication(copyOnRead = true)
@@ -138,6 +72,71 @@ public class ExampleSpringDataGeodeApplication implements Runnable {
 		GemfireTemplate usersTemplate(GemFireCache cache) {
 			return new GemfireTemplate(cache.getRegion(GemfireUtils.toRegionPath(USERS_REGION_NAME)));
 		}
+	}
+
+	private static @NonNull ConfigurableApplicationContext newApplicationContext(Class<?>... configurationClasses) {
+
+		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+
+		applicationContext.register(configurationClasses);
+		applicationContext.registerShutdownHook();
+		applicationContext.refresh();
+
+		return applicationContext;
+	}
+
+	private final ConfigurableApplicationContext applicationContext;
+
+	private final String[] arguments;
+
+	public ExampleSpringDataGeodeApplication(@NonNull String... arguments) {
+		this(newApplicationContext(ApplicationConfiguration.class), arguments);
+	}
+
+	public ExampleSpringDataGeodeApplication(@NonNull ConfigurableApplicationContext applicationContext,
+			@NonNull String... arguments) {
+
+		Assert.notNull(applicationContext, "Spring ApplicationContext is required");
+
+		this.applicationContext = applicationContext;
+		this.arguments = arguments;
+	}
+
+	protected @NonNull ConfigurableApplicationContext getApplicationContext() {
+		return this.applicationContext;
+	}
+
+	protected @NonNull String[] getArguments() {
+		return this.arguments != null ? this.arguments : new String[0];
+	}
+
+	protected void log(String message, Object... arguments) {
+		System.out.printf(message, arguments);
+		System.out.flush();
+	}
+
+	@Override
+	public void run() {
+		run(getApplicationContext().getBean("usersTemplate", GemfireTemplate.class), getArguments());
+	}
+
+	protected void run(@NonNull GemfireTemplate usersTemplate, @NonNull String[] arguments) {
+
+		User jonDoe = User.of("jonDoe").identifiedBy(1L);
+
+		log("Saving User [%s]...%n", jonDoe);
+
+		usersTemplate.put(jonDoe.getId(), jonDoe);
+
+		log("Finding User [%s] by ID [%d]...%n", jonDoe, jonDoe.getId());
+
+		User loadedJonDoe = usersTemplate.get(jonDoe.getId());
+
+		assertThat(loadedJonDoe).isNotNull();
+		assertThat(loadedJonDoe).isNotSameAs(jonDoe);
+		assertThat(loadedJonDoe).isEqualTo(jonDoe);
+
+		log("SUCCESS!!%n");
 	}
 
 	@Getter
