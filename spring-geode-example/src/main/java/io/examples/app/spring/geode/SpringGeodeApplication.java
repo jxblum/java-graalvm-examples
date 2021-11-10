8cd43ad9e42a6792c4de39ca89a1fb5365a96728
John Blum <jblum@vmware.com>
2021-11-09 20:26:13 -0800

    Add separate Java and Annotation based configurations for Spring Boot for Apache Geode application.
    Use the SpringApplicationBuilder to construct, configure and initialize (run) the Spring Geode application.
    
    Rename the User.of(name) static factory method to User.as(name).
    
    Add log(message, args) method to log to the system standard out.

--- spring-geode-example/src/main/java/io/examples/app/spring/geode/SpringGeodeApplication.java
+++ spring-geode-example/src/main/java/io/examples/app/spring/geode/SpringGeodeApplication.java
@@ -2,13 +2,17 @@ package io.examples.app.spring.geode;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.apache.geode.cache.GemFireCache;
 import org.apache.geode.cache.client.ClientRegionShortcut;
 
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.gemfire.client.ClientRegionFactoryBean;
 import org.springframework.data.gemfire.config.annotation.EnableEntityDefinedRegions;
 import org.springframework.data.gemfire.mapping.annotation.Region;
 import org.springframework.data.repository.CrudRepository;
@@ -25,11 +29,16 @@ import lombok.Setter;
  * A {@link SpringBootApplication} (SBDG) using Apache Geode to save (persist) and load (read) data.
  *
  * @author John Blum
+ * @see org.apache.geode.cache.GemFireCache
  * @see org.springframework.boot.ApplicationRunner
  * @see org.springframework.boot.SpringApplication
  * @see org.springframework.boot.autoconfigure.SpringBootApplication
+ * @see org.springframework.boot.builder.SpringApplicationBuilder
  * @see org.springframework.context.annotation.Bean
  * @see org.springframework.context.annotation.Configuration
+ * @see org.springframework.context.annotation.Profile
+ * @see org.springframework.data.gemfire.client.ClientRegionFactoryBean
+ * @see org.springframework.data.gemfire.config.annotation.EnableEntityDefinedRegions
  * @see org.springframework.data.repository.CrudRepository
  * @since 1.0.0
  */
@@ -38,32 +47,59 @@ import lombok.Setter;
 public class SpringGeodeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringGeodeApplication.class);
+
+		new SpringApplicationBuilder(SpringGeodeApplication.class)
+			.profiles("java-configuration")
+			.web(WebApplicationType.NONE)
+			.build()
+			.run(args);
 	}
 
 	@Configuration
+	@Profile("annotation-configuration")
 	@EnableEntityDefinedRegions(basePackageClasses = User.class, clientRegionShortcut = ClientRegionShortcut.LOCAL)
-	static class ApplicationConfiguration { }
+	static class AnnotationApplicationConfiguration { }
+
+	@Configuration
+	@Profile("java-configuration")
+	static class JavaApplicationConfiguration {
+
+		@Bean("Users")
+		public ClientRegionFactoryBean<Object, Object> usersRegion(GemFireCache gemfireCache) {
+
+			ClientRegionFactoryBean<Object, Object> clientRegion = new ClientRegionFactoryBean<>();
+
+			clientRegion.setCache(gemfireCache);
+			clientRegion.setShortcut(ClientRegionShortcut.LOCAL);
+
+			return clientRegion;
+		}
+	}
+
+	protected void log(String message, Object... arguments) {
+		System.out.printf(message, arguments);
+		System.out.flush();
+	}
 
 	@Bean
 	@NonNull ApplicationRunner runner(@NonNull UserRepository userRepository) {
 
 		return args -> {
 
-			User jonDoe = User.of("jonDoe").identifiedBy(1L);
+			User jonDoe = User.as("jonDoe").identifiedBy(1L);
 
-			System.out.printf("Saving [%s]...%n", jonDoe);
+			log("Saving User [%s]...%n", jonDoe);
 
 			userRepository.save(jonDoe);
 
 			User loadedJonDoe = userRepository.findById(jonDoe.getId()).orElse(null);
 
-			System.out.printf("Loaded [%s]%n", loadedJonDoe);
+			log("Loaded User [%1$s] with ID [%2$d]%n", loadedJonDoe, jonDoe.getId());
 
 			assertThat(loadedJonDoe).isNotNull();
 			assertThat(loadedJonDoe).isEqualTo(jonDoe);
 
-			System.out.println("SUCCESS!!");
+			log("SUCCESS!!%n");
 		};
 	}
 }
@@ -71,7 +107,7 @@ public class SpringGeodeApplication {
 @Getter
 @Region("Users")
 @EqualsAndHashCode
-@RequiredArgsConstructor(staticName = "of")
+@RequiredArgsConstructor(staticName = "as")
 class User {
 
 	@Setter(AccessLevel.PRIVATE)
